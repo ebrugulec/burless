ce8297a99b85273bd9a9dfb2d04a73ce40a9c3d9
ebrugulec <gulecebrueg@gmail.com>
2021-02-22 19:55:46 +0300

    Protected nav

---
+++ config/unauthenticatedNavButtons.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const authenticatedNavButtons = [
+  {
+    label: 'Links',
+    path: '/',
+  },
+  {
+    label: 'Statistic',
+    path: '/statistic',
+  },
+  {
+    label: 'Profile',
+    path: '/profile',
+  },
+  {
+    label: 'Contact',
+    path: '/contact',
+  },
+  {
+    label: 'Login',
+    path: '/login',
+  },
+]
+
+export default authenticatedNavButtons
