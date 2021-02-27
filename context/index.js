7ce732a7b5e7b94792f2ea30e6e54f506651bfc2
ebrugulec <gulecebrueg@gmail.com>
2021-02-27 22:45:37 +0300

    remove comment lines

--- context/index.js
+++ context/index.js
@@ -1,10 +1,4 @@
-import { useState, useEffect, useReducer, createContext } from "react";
-import { user } from "./reducers/user";
-
-// const initialState = {
-//   user: {},
-//   loggedIn: false
-// };
+import { createContext } from "react";
 
 const Context = createContext({});
 
@@ -12,12 +6,5 @@ const combineReducers = (...reducers) => (state, action) => {
   for (let i = 0; i < reducers.length; i++) state = reducers[i](state, action);
   return state;
 };
-
-// const Provider = ({ children }) => {
-//   const [state, dispatch] = useReducer(combineReducers(user), initialState);
-//   const value = { state, dispatch };
-//
-//   return <Context.Provider value={value}>{children}</Context.Provider>;
-// };
 
 export { Context, combineReducers };
