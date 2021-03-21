b309fb3817a04c12aa35b60f20777788c5ba5f8e
ebrugulec <gulecebrueg@gmail.com>
2021-03-21 16:49:32 +0300

    add prefix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
+const withPlugins = require("next-compose-plugins");
+const withImages = require('next-images');
 const withSass = require('@zeit/next-sass')
-const withImages = require('next-images')
-module.exports = withSass({
-  distDir: "_next",
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
-})
+
+module.exports = withPlugins([[withSass({
+  assetPrefix: ".",
+}), withImages()]]);
