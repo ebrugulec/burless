8b8cd8df0f7d828e70ba5ccf51349ca75bbdd25a
ebrugulec <gulecebrueg@gmail.com>
2021-03-21 12:53:18 +0300

    Add image and style to dashboard

--- components/Layout/Header/Header.js
+++ components/Layout/Header/Header.js
@@ -1,19 +1,25 @@
 import Link from 'next/link'
 // import Image from 'next/image';
 // import BurlessLogo from "../../../public/burless-logo.png";
+// import logo from "../../../public/logo.png";
+import Image from 'next/image'
 
 // import style from "./Header.module.scss";
 import React from 'react'
 
-const Header = (props) => (
-  <Link href="/">
-    {/*<Image*/}
-    {/*  src="/burless-logo.png"*/}
-    {/*  alt="BurlessLogo"*/}
-    {/*  width={90}*/}
-    {/*  height={90}*/}
-    {/*/>*/}
-  </Link>
-)
-
-export default Header
+function Header () {
+  return (
+    <div className="header">
+      <Image src="/logo.svg"
+       width={77}
+       height={30}
+      />
+      <Image
+        src="/hamburger.svg"
+       width={24}
+       height={24}
+      />
+    </div>
+  )
+}
+export default Header;
